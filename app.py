ccde1885f12d306f698ddde9eea351641a49bda0
ArisuAbhilash <mauryaabhilash912@gmail.com>
2025-07-09 09:56:58 +0530

    fix: issues in dockerfile

--- app.py
+++ app.py
@@ -64,4 +64,4 @@ def index():
     return render_template('index.html', weather=weather, forecast=forecast)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='localhost', port=4449, debug=True)
